28338db68a4a2a9c5bae3509c0c782207dc9f4aa
Jose <almajano1981@gmail.com>
2022-02-10 18:54:49 +0100

    animaciones balas, script sonido, creacion escenarios

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -1,20 +1,50 @@
+
+   
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulletScript : MonoBehaviour
+public class BulletScript : MonoBehaviour
 {
-   public float Speed;
-    
-   private Rigidbody2D Rigidbody2D;
+    public float Speed;
+    public AudioClip Sound;
 
-    void Start()
+    private Rigidbody2D Rigidbody2D;
+    private Vector3 Direction;
+
+    private void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Camera.main.GetComponent<AudioSource>().PlayOneShot(Sound);
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = Vector2.right * Speed;
+        Rigidbody2D.velocity = Direction * Speed;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        Direction = direction;
+    }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GruntScript grunt = other.GetComponent<GruntScript>();
+        MovementJonh john = other.GetComponent<MovementJonh>();
+        if (grunt != null)
+        {
+            grunt.Hit();
+        }
+        if (john != null)
+        {
+            john.Hit();
+        }
+        DestroyBullet();
     }
 }
--- Assets/Scripts/MovementJonh.cs
+++ Assets/Scripts/MovementJonh.cs
@@ -4,47 +4,58 @@ using UnityEngine;
 
 public class MovementJonh : MonoBehaviour
 {
-    public GameObject BulletPrefab;
     public float Speed;
     public float JumpForce;
+    public GameObject BulletPrefab;
 
-    public Rigidbody2D Rigidbody2D;
+    private Rigidbody2D Rigidbody2D;
     private Animator Animator;
     private float Horizontal;
     private bool Grounded;
+    private float LastShoot;
+    private int Health = 5;
 
-    void Start()
+    private void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
-        Animator =GetComponent<Animator>();
+        Animator = GetComponent<Animator>();
     }
 
-    
-    void Update()
+    private void Update()
     {
-       Horizontal =Input.GetAxisRaw("Horizontal");
+        // Movimiento
+        Horizontal = Input.GetAxisRaw("Horizontal");
 
-       if(Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-       else if(Horizontal >0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
-       Animator.SetBool("running", Horizontal !=0.0f);
+        Animator.SetBool("running", Horizontal != 0.0f);
 
-        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
-       if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        // Detectar Suelo
+        // Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
             Grounded = true;
         }
         else Grounded = false;
-      
-       if(Input.GetKeyDown(KeyCode.W) && Grounded)
-       {
-           Jump();
-       }
 
-       if(Input.GetKeyDown(KeyCode.Space))
-       {
-           Shoot();
-       }
+        // Salto
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+        }
+
+        // Disparar
+        if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
 
     private void Jump()
@@ -55,17 +66,16 @@ public class MovementJonh : MonoBehaviour
     private void Shoot()
     {
         Vector3 direction;
-        if(transform.localScale.x ==1.0f) direction = Vector3.right;
+        if (transform.localScale.x == 1.0f) direction = Vector3.right;
         else direction = Vector3.left;
-
 
         GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
         bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
 
-    private void FixedUpdate()
+    public void Hit()
     {
-
-        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+        Health -= 1;
+        if (Health == 0) Destroy(gameObject);
     }
 }
